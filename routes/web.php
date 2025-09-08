3b004bef3e2acf2ad926d88f8c635daddd801c70
MadeSugi <imadesugih@gmail.com>
2025-09-08 09:44:41 +0700

    Update web.php
    Co-Authored-By: Asyasimarmata <169507680+Asyasimarmata@users.noreply.github.com>
    Co-Authored-By: ImanuelYayan <170066918+ImanuelYayan@users.noreply.github.com>
    Co-Authored-By: estradwisaputra-blip <230102360+estradwisaputra-blip@users.noreply.github.com>
    Co-Authored-By: estyyA <169512047+estyyA@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,23 @@
 <?php
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('Login');
 });
 
-//bagian register&login admin
-//Route::get('/registeradmin', [PageController::class, 'registerAdminform'])->name('registeradmin');
-//Route::post('/registeradmin', [PageController::class, 'registerAdmin'])->name('register.admin');
-
-
-// tampilkan form login karyawan
+// Login
 Route::get('/Login', [PageController::class, 'Login'])->name('Login');
-
-// proses login karyawan (sementara kosong dulu)
 Route::post('/Login', [PageController::class, 'Login'])->name('Login');
 
-// arahkan ke PageController bagian dashboard&lain-lain
+// Dashboard & Halaman lain
 Route::get('/dashboard', [PageController::class, 'dashboard']);
 Route::get('/daftarPresensi', [PageController::class, 'daftarPresensi']);
 Route::get('/daftarKaryawan', [PageController::class, 'daftarKaryawan']);
 Route::get('/laporan', [PageController::class, 'laporan']);
-
-
-// untuk presensi karyawan
 Route::get('/PresensiKaryawan', [PageController::class, 'PresensiKaryawan']);
+
+// Laporan (PDF & Excel)
+Route::get('/laporan/pdf', [PageController::class, 'cetakPdf'])->name('laporan.cetakPdf');
+Route::get('/laporan/excel', [PageController::class, 'exportExcel'])->name('laporan.exportExcel');
