f6ed0b07de4b9c9ca9e7a2e4abe10edbe4a0325d
estyyA <resty.dwi@si.ukdw.ac.id>
2025-09-10 10:27:51 +0700

    Create AkunSeeder.php

---
+++ database/seeds/AkunSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AkunSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            ['NIK' => 'EMP001', 'username' => 'budi', 'password' => Hash::make('123456')],
+            ['NIK' => 'EMP002', 'username' => 'sari', 'password' => Hash::make('123456')],
+            ['NIK' => 'EMP003', 'username' => 'andi', 'password' => Hash::make('123456')],
+            ['NIK' => 'EMP004', 'username' => 'rina', 'password' => Hash::make('123456')],
+            ['NIK' => 'EMP005', 'username' => 'agus', 'password' => Hash::make('123456')],
+        ];
+
+        foreach ($data as $akun) {
+            DB::table('akun')->updateOrInsert(
+                ['username' => $akun['username']], // cek dulu berdasarkan username
+                $akun
+            );
+        }
+    }
+}
