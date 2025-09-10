48bf05f69841fffca1f2270627e9954fe51d5323
MadeSugi <imadesugih@gmail.com>
2025-09-10 10:10:54 +0700

    Create Department.php

---
+++ app/Department.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $table = 'Departement';
+    protected $primaryKey = 'id_divisi';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama_divisi',
+    ];
+
+    // Relasi ke Karyawan
+    public function karyawans()
+    {
+        return $this->hasMany(Karyawan::class, 'id_divisi', 'id_divisi');
+    }
+}
