1cb1ef18fe7ca256b01f095008c08026c4faf8b0
MadeSugi <imadesugih@gmail.com>
2025-09-10 10:10:57 +0700

    Create Akun.php

---
+++ app/Akun.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Models\Karyawan;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Akun extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'akun';
+    protected $primaryKey = 'username';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'username',
+        'NIK',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // Relasi ke Karyawan
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class, 'NIK', 'NIK');
+    }
+}
