791b43d726376fc087c4ca5d2d41897a7e3b807c
estyyA <resty.dwi@si.ukdw.ac.id>
2025-09-17 08:44:55 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,21 +8,21 @@
         <div class="card card-stat shadow-sm">
             <i class="fa fa-user-circle text-primary"></i>
             <div>Jumlah Karyawan</div>
-            <h3>20</h3>
+            <h3>{{ $totalKaryawan }}</h3>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card card-stat shadow-sm">
             <i class="fa fa-user-check text-success"></i>
             <div>Karyawan Masuk</div>
-            <h3>17</h3>
+            <h3>{{ $karyawanMasuk }}</h3>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card card-stat shadow-sm">
             <i class="fa fa-user-times text-danger"></i>
             <div>Karyawan Izin/Cuti</div>
-            <h3>3</h3>
+            <h3>{{ $karyawanIzin }}</h3>
         </div>
     </div>
 </div>
@@ -30,7 +30,6 @@
 <div class="card mt-4 shadow-sm">
     <div class="card-header font-weight-bold">Data Karyawan</div>
     <div class="card-body d-flex justify-content-center">
-        {{-- Atur ukuran canvas --}}
         <div style="max-width: 300px; max-height: 300px;">
             <canvas id="chartKaryawan"></canvas>
         </div>
@@ -46,7 +45,7 @@
         data: {
             labels: ['Masuk', 'Izin/Cuti'],
             datasets: [{
-                data: [17, 3],
+                data: [{{ $karyawanMasuk }}, {{ $karyawanIzin }}],
                 backgroundColor: ['#11ab59ff', '#d81017ff']
             }]
         },
@@ -54,9 +53,7 @@
             responsive: true,
             maintainAspectRatio: false,
             plugins: {
-                legend: {
-                    position: 'bottom', // biar legend rapi
-                }
+                legend: { position: 'bottom' }
             }
         }
     });
